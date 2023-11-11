4af8e48d6325adb1f5674ead36d49d40fb25ded1
Azamat <agiztdinov45@gmaail.com>
2023-11-11 14:45:36 +0300

    ddcaemdwmsdwx

---
+++ express/pyth.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
+
+@app.route('/process', methods=['POST'])
+def process_video():
+    try:
+        video_data = request.data
+        # Здесь обработайте видео в вашей нейронной сети
+        # и получите результаты в формате JSON
+        # В данном примере просто возвращаем случайные результаты
+        result = {'prediction': 'catcatcatcat'}
+        return jsonify(result)
+    except Exception as e:
+        print(str(e))
+        return jsonify({'error': 'Internal Server Error'}), 500
+
+# Запуск сервера Flask
+if __name__ == '__main__':
+    app.run(port=5000)
